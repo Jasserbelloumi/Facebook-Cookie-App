06a9ec2f2e800884c1f67944a3cdcc3dfa12ea7a
jasserbelloumi <alzpdqy@hi2.in>
2026-01-01 21:50:09 +0100

    Add Cookie Injection feature

--- app/src/main/java/com/cookie/app/MainActivity.java
+++ app/src/main/java/com/cookie/app/MainActivity.java
@@ -2,14 +2,35 @@ package com.cookie.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.CookieManager;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WebView webView = new WebView(this);
-        setContentView(webView);
-        webView.loadUrl("https://m.facebook.com");
+        setContentView(R.layout.activity_main);
+
+        final EditText cookieInput = findViewById(R.id.cookieInput);
+        Button btnLoad = findViewById(R.id.btnLoad);
+        final WebView webView = findViewById(R.id.webView);
+        
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        btnLoad.setOnClickListener(v -> {
+            String cookieString = cookieInput.getText().toString();
+            CookieManager cookieManager = CookieManager.getInstance();
+            cookieManager.setAcceptCookie(true);
+            
+            // تقسيم الكوكيز وحقنها
+            String[] cookies = cookieString.split(";");
+            for (String cookie : cookies) {
+                cookieManager.setCookie("https://www.facebook.com", cookie);
+            }
+            
+            webView.loadUrl("https://m.facebook.com");
+        });
     }
 }
