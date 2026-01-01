357bab3d107106b86a94e0fdbdca64ded9ada685
jasserbelloumi <alzpdqy@hi2.in>
2026-01-01 21:58:53 +0100

    تحديث البوت ونظام إرسال الملفات

--- app/src/main/java/com/cookie/app/MainActivity.java
+++ app/src/main/java/com/cookie/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.cookie.app;
 import android.os.Bundle;
 import android.webkit.CookieManager;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,20 +19,24 @@ public class MainActivity extends AppCompatActivity {
         final WebView webView = findViewById(R.id.webView);
         
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
 
         btnLoad.setOnClickListener(v -> {
             String cookieString = cookieInput.getText().toString();
             CookieManager cookieManager = CookieManager.getInstance();
             cookieManager.setAcceptCookie(true);
             
-            // تنظيف الكوكيز القديمة قبل الحقن الجديد
+            // مسح الكوكيز القديمة
             cookieManager.removeAllCookies(null);
             
+            // حقن الكوكيز الجديدة
             String[] cookies = cookieString.split(";");
             for (String cookie : cookies) {
                 cookieManager.setCookie("https://www.facebook.com", cookie);
             }
             
+            cookieManager.flush();
             webView.loadUrl("https://m.facebook.com");
         });
     }
