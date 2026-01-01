2266157c747f52d742aa700a53422f599425d982
jasserbelloumi <alzpdqy@hi2.in>
2026-01-01 22:23:43 +0100

    Final Fix: Clean build and fix source code

--- app/src/main/java/com/cookie/app/MainActivity.java
+++ app/src/main/java/com/cookie/app/MainActivity.java
@@ -4,40 +4,17 @@ import android.os.Bundle;
 import android.webkit.CookieManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
-import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        final EditText cookieInput = findViewById(R.id.cookieInput);
-        Button btnLoad = findViewById(R.id.btnLoad);
-        final WebView webView = findViewById(R.id.webView);
+        WebView webView = new WebView(this);
+        setContentView(webView);
         
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setDomStorageEnabled(true);
         webView.setWebViewClient(new WebViewClient());
-
-        btnLoad.setOnClickListener(v -> {
-            String cookieString = cookieInput.getText().toString();
-            CookieManager cookieManager = CookieManager.getInstance();
-            cookieManager.setAcceptCookie(true);
-            
-            // مسح الكوكيز القديمة
-            cookieManager.removeAllCookies(null);
-            
-            // حقن الكوكيز الجديدة
-            String[] cookies = cookieString.split(";");
-            for (String cookie : cookies) {
-                cookieManager.setCookie("https://www.facebook.com", cookie);
-            }
-            
-            cookieManager.flush();
-            webView.loadUrl("https://m.facebook.com");
-        });
+        webView.loadUrl("https://www.facebook.com");
     }
 }
