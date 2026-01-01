b636abf607dcd01a7a29088d4d6459844602194d
jasserbelloumi <alzpdqy@hi2.in>
2026-01-01 22:37:49 +0100

    Add Cookie Injection feature

--- app/src/main/java/com/cookie/app/MainActivity.java
+++ app/src/main/java/com/cookie/app/MainActivity.java
@@ -4,17 +4,51 @@ import android.os.Bundle;
 import android.webkit.CookieManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    EditText cookieInput;
+    Button btnInject;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WebView webView = new WebView(this);
-        setContentView(webView);
-        
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        cookieInput = findViewById(R.id.cookieInput);
+        btnInject = findViewById(R.id.btnInject);
+
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.facebook.com");
+
+        btnInject.setOnClickListener(v -> {
+            String fullCookie = cookieInput.getText().toString();
+            if (!fullCookie.isEmpty()) {
+                injectCookie(fullCookie);
+            } else {
+                Toast.makeText(this, "الرجاء وضع الكوكيز أولاً!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        webView.loadUrl("https://m.facebook.com");
+    }
+
+    public void injectCookie(String cookieString) {
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+        
+        // تقسيم الكوكيز وحقنها
+        String[] cookies = cookieString.split(";");
+        for (String cookie : cookies) {
+            cookieManager.setCookie("https://.facebook.com", cookie);
+        }
+        
+        Toast.makeText(this, "تم الحقن! جاري التحديث...", Toast.LENGTH_SHORT).show();
+        webView.reload();
     }
 }
